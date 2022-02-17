19d4e8120b95ac29250885e39dfe00f82aa41508
Jeff Weiss <jmw182@pitt.edu>
2022-02-17 18:11:34 -0500

    Added support for cell inputs.

--- +ERAASR/+RNEL/apply_blanking.m
+++ +ERAASR/+RNEL/apply_blanking.m
@@ -1,9 +1,19 @@
 function data_blanked = apply_blanking(data_tensor, stim_tensor, varargin)
 % ERAASR.RNEL.apply_blanking(data_tensor, stim_tensor, ...)
-% data_tensor is nTrials x nTime x nChannels tensor 
-% (also support nTrials x 1 cell of nTime x nChannels matrices??)
-% stim_tensor is nTrials x nPulses matrix
-% (also support nTrials x 1 cell of nPulses x 1 vectors??)
+% data_tensor is either:
+%  - nTrials x 1 cell of time x channels matrices
+%  - nTrials x nTime x channels tensor
+% stim_tensor is either
+%  - nTrials x 1 cell of nPulses x 1 vectors
+%  - nTrials x nPulses matrix
+%
+% Optional inputs include:
+% blank_samples (how many samples to blank, default: 48)
+% method ('linear_ramp' or 'sample_and_hold', default: 'linear_ramp')
+%       blank_offset (how many samples to offset the stim_tensor times by to
+%       initiate blanking. Can be positive (delays) or negative (precedes).
+%       Default: 2)
+% showProgress (default: false)
 
 % parse inputs
 p = inputParser();
@@ -23,47 +33,107 @@ if blank_samples == 0
     return
 end
 
+
+f_dataCell = iscell(data_tensor);
+f_stimCell = iscell(stim_tensor);
+%f_cell = f_dataCell || f_stimCell;
+% if either input is a cell, make sure both are
+% if f_cell
+%     if f_dataCell && ~f_stimCell % convert stimTensor to cell
+%         stim_tensor = num2cell(stim_tensor, 2); % split trials into cell of cells
+%         stim_tensor = cellfun(@(x) x{:}', stim_tensor, 'UniformOutput', false); % extract innermost cells and transpose to match documentation
+%         f_stimCell = true;
+%     elseif f_stimCell && ~f_dataCell % convert dataTensor to cell
+%         data_tensor = num2cell(data_tensor, [2 3]); % split trials into cell of cells
+%         data_tensor = cellfun(@(x) x{:}', data_tensor, 'UniformOutput', false); % extract innermost cells and transpose to match documentation
+%         data_blanked = data_tensor;
+%         f_dataCell = true;
+%     end
+% end
+
 % apply blank_offset to stim_tensor
-stim_tensor = stim_tensor + blank_offset;
+if f_stimCell
+    stim_tensor = cellfun(@(x) x + blank_offset, stim_tensor, 'UniformOutput', false);
+else
+    stim_tensor = stim_tensor + blank_offset;
+end
 
 % determine number of trials and pulses
-[nTrials, nPulses] = size(stim_tensor);
-nChans = size(data_tensor, 3);
+if f_stimCell
+    nTrials = length(stim_tensor);
+    nPulses = cellfun('length', stim_tensor); % vector of number of pulses per trial
+else
+    [nTrials, nPulses] = size(stim_tensor);
+    nPulses = repmat(nPulses, 1, nTrials); % expand to match format for f_stimCell
+end
+
+if f_dataCell
+    nChans = cellfun(@(x) size(x,2), data_tensor); % vector of number of channels per trial
+else
+    nChans = size(data_tensor, 3);
+    nChans = repmat(nChans, 1, nTrials); % expand to match format for f_dataCell
+end
 
 % loop over trials and pulses and apply blanking
 if showProgress
-    prog = ERAASR.Utils.ProgressBar(nTrials*nPulses, 'Applying blanking to data');
+    prog = ERAASR.Utils.ProgressBar(sum(nPulses), 'Applying blanking to data');
+    iCount = 0;
 end
 
-iCount = 0;
+
 for iTrial = 1:nTrials
-    for iPulse = 1:nPulses
-        iCount = iCount+1;
+    for iPulse = 1:nPulses(iTrial)
+
         if showProgress
+            iCount = iCount+1;
             prog.update(iCount);
         end
-        
-        % data_tensor is nTrials x nTime x nChannels tensor
-        % stim_tensor is nTrials x nPulses matrix
-        p0 = stim_tensor(iTrial, iPulse); % first sample to blank
+
+        if f_stimCell % stim_tensor is nTrials x 1 cell of nPulses x 1 vectors
+            p0 = stim_tensor{iTrial}(iPulse); % first sample to blank
+        else % stim_tensor is nTrials x nPulses matrix
+            p0 = stim_tensor(iTrial, iPulse); % first sample to blank
+        end
         p_1 = p0 - 1; % last sample before blanking
         pEnd = p0 + blank_samples - 1;
         pIdx = p0:pEnd; % sample index to blank for this pulse
-        switch method
-            case 'sample_and_hold' 
-                data_blanked(iTrial, pIdx, :) = repmat(data_tensor(iTrial, p_1, :), 1, blank_samples, 1);
-            case 'linear_ramp'
-                v0 = squeeze(data_tensor(iTrial, p_1, :));
-                vEnd = squeeze(data_tensor(iTrial, pEnd + 1, :));
-                for iChan = 1:nChans
-                    data_blanked(iTrial, pIdx, iChan) = linspace(v0(iChan), vEnd(iChan), blank_samples);
-                end
-            otherwise
-                error('blank method not implemented.');
+
+        if f_dataCell
+            % data_tensor is nTrials x 1 cell of nTime x nChannels matrices
+
+            switch method
+                case 'sample_and_hold'
+                    data_blanked{iTrial}(pIdx, :) = repmat(data_tensor{iTrial}(p_1, :), blank_samples, 1);
+                case 'linear_ramp'
+                    v0 = data_tensor{iTrial}(p_1, :);
+                    vEnd = data_tensor{iTrial}(pEnd + 1, :);
+                    for iChan = 1:nChans(iTrial)
+                        data_blanked{iTrial}(pIdx, iChan) = linspace(v0(iChan), vEnd(iChan), blank_samples);
+                    end
+                otherwise
+                    error('blank method not implemented.');
+            end
+
+        else % tensors (not cells)
+            % data_tensor is nTrials x nTime x nChannels tensor
+
+            switch method
+                case 'sample_and_hold'
+                    data_blanked(iTrial, pIdx, :) = repmat(data_tensor(iTrial, p_1, :), 1, blank_samples, 1);
+                case 'linear_ramp'
+                    v0 = squeeze(data_tensor(iTrial, p_1, :));
+                    vEnd = squeeze(data_tensor(iTrial, pEnd + 1, :));
+                    for iChan = 1:nChans(iTrial)
+                        data_blanked(iTrial, pIdx, iChan) = linspace(v0(iChan), vEnd(iChan), blank_samples);
+                    end
+                otherwise
+                    error('blank method not implemented.');
+            end
         end
 
-    end
-end
+    end % end pulse loop
+end % end trial loop
+
 if showProgress
     prog.finish();
 end
